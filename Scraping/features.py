95d3be9af24aa17aec40a62f2baa5b1e5704ddb6
Danylo-D87 <quaestor88@gmail.com>
2025-07-09 19:12:27 +0300

    information is in json

---
+++ Scraping/features.py
@@ -0,0 +1,31 @@
+import json
+import re
+from contextlib import contextmanager
+from typing import List
+
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+
+@contextmanager
+def get_driver():
+    chrome_options = Options()
+    driver = webdriver.Chrome(options=chrome_options)
+    try:
+        yield driver
+    finally:
+        driver.quit()
+
+
+def write_products_to_json(products: List) -> None:
+
+    with open("../products.json", "w", encoding="utf-8") as f:
+        json.dump(products, f, ensure_ascii=False, indent=4)
+
+
+def extract_float(text: str) -> float | None:
+    if not text:
+        return None
+    first_line = text.split('\n')[0].strip()
+    match = re.search(r"[\d.,]+", first_line)
+    return float(match.group(0).replace(',', '.')) if match else None
